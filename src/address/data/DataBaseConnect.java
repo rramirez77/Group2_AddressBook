a227ebb5d3a8cc47cff8a13d2b4b78e15332a3cf
rramirez77 <70995300+rramirez77@users.noreply.github.com>
2022-03-11 17:16:22 -0800

    Connected to the server. Still need to create the table.

--- src/address/data/DataBaseConnect.java
+++ src/address/data/DataBaseConnect.java
@@ -1,4 +1,7 @@
 package address.data;
+
+// You need to import the java.sql package to use JDBC
+
 import java.sql.*;
 
 public class DataBaseConnect {
@@ -8,7 +11,7 @@ public class DataBaseConnect {
     public static void main(String[] args) throws SQLException, ClassNotFoundException{
 
         // Load the Oracle JDBC driver
-        Class.forName ("oracle.jdbc.OracleDriver"); //name of driver may change w/ versions
+        Class.forName ("oracle.jdbc.driver.OracleDriver"); //name of driver may change w/ versions
 
         //check Oracle documentation online
         // Or could do DriverManager.registerDriver (new oracle.jdbc.OracleDriver());
@@ -20,34 +23,28 @@ public class DataBaseConnect {
         // were given by our CS tech in an email ---THIS WILL BE DIFFERENT
         //jdbc:oracle:thin:@//adcsdb01.csueastbay.edu:1521/mcspdb.ad.csueastbay.edu
         Connection conn =
-                DriverManager.getConnection("jdbc:oracle:thin:YOUR_LOGIN/YOUR_PASSWORD@adcsdb01.csueastbay.edu:1521/mcspdb.ad.csueastbay.edu");
+                DriverManager.getConnection("jdbc:oracle:thin:mcs1015/FSE8ZFGm@adcsdb01.csueastbay.edu:1521/mcspdb.ad.csueastbay.edu");
 
         // Create a Statement
         Statement stmt = conn.createStatement ();
 
-
-
         // Select the all (*) from the table JAVATEST
-
         ResultSet rset = stmt.executeQuery("SELECT * FROM DOGS");
 
         System.out.println(rset);
 
-
-
-
-
         // Iterate through the result and print the employee names
 
         while (rset.next ()) //get next row of table returned
+        {
+            for(int i=1; i<=rset.getMetaData().getColumnCount(); i++) { //visit each column
 
-        {         for(int i=1; i<=rset.getMetaData().getColumnCount(); i++) //visit each column
+                System.out.print(rset.getString(i) + " | ");
 
-            System.out.print(rset.getString(i) + " | ");
+                System.out.println(" ");
 
-            System.out.println(" ");
-
-            System.out.println("========================================");
+                System.out.println("========================================");
+            }
 
         }
 
